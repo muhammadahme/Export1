a1e463d8e8990c174ebd5ee88a04f8f52ffbe029
"iqbal ahmed" <iqbalahmedusa@gmail.com>
2020-04-28 03:09:37 -0400

    my apple count

---
+++ Apple/src/count/AddApple.java
@@ -0,0 +1,13 @@
+package count;
+
+public class AddApple {
+
+	public static void main(String[] args) {
+		int appleBox1 = 100;
+		int appleBox2 = 300;
+		int addApple =appleBox1 + appleBox2;
+		System.out.println(addApple);
+
+	}
+
+}
